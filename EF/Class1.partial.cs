634ac22a26afc428af4e762eef32814aaa1a33c2
ray155093 <ray155093@gmail.com>
2016-05-14 10:09:41 +0800

    第一個練習

---
+++ EF/Class1.partial.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF
+{
+    public partial class Class1
+    {
+        partial void init()
+        {
+            string a = "!";
+        }
+    }
+}
---
+++ EF/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string a = null;
+            string test = a ?? "test";
+
+            /*
+            int? b = a.Length;
+            //C# 6.0 新功能
+            //int? b = a?.Length;
+            */
+
+            using (var db = new ContosoUniversityEntities())
+            {
+                foreach (var item in db.Course)
+                {
+                    Console.WriteLine(item.Title);
+ 
+                }
+            }
+            Console.WriteLine("Hello");
+        }
+
+    }
+}
