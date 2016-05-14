af2b06eceaecb22d1b2422430ee6f68195298e0f
ray155093 <ray155093@gmail.com>
2016-05-14 12:35:18 +0800

    程式重構 修改流程

--- EF/Program.cs
+++ EF/Program.cs
@@ -20,82 +20,150 @@ namespace EF
             //int? b = a?.Length;
             */
 
-            
+
             using (var db = new ContosoUniversityEntities())
             {
-                //基本
-                foreach (var item in db.Course)
-                {
-                    Console.WriteLine(item.Title);
 
-                }
-                
-                Console.WriteLine("\n");
-
-                //取出課程名稱中有Git的 用LINQ
-                var data = from p in db.Course
-                           where p.Title.Contains("Git")
-                           select p;
-                foreach (var item in data)
-                {
-                    Console.WriteLine(item.Title);
-
-                }
-                Console.WriteLine("\n");
-
-                //新增欄位
-                db.Course.Add(new Course()
-                {
-                    Title="Git Test",
-                    Credits=5,
-                    DepartmentID=1
-                });
-                
-
-                //新增欄位
-                var c = new Course()
-                {
-                    Title = "Git Test 2",
-                    Credits = 5,
-                };
-                c.Department = db.Department.Find(2);
-
-                //更新前
-                Console.WriteLine(c.DepartmentID);
-               
-                db.Course.Add(c);
-                //更新後
-                db.SaveChanges();
-                Console.WriteLine(c.DepartmentID);
-
+                Select(db);
+                Console.WriteLine("------");
+                SelectByLinq(db);
+                Console.WriteLine("------");
+                Insert(db);
+                Console.WriteLine("------");
+                InsertByFind(db);
+                Console.WriteLine("------");
+                Delete(db);
+                Console.WriteLine("------");
+                Select(db);
                 Console.ReadLine();
 
+                Select(db);
 
-                foreach (var item in data)
-                {
-                    Console.WriteLine(item.Title);
 
-                }
 
-                //刪除資料
-                /*
-                 * 此句會刪除一筆最新的資料 但不是CourseID編號12 new時並沒有在new時被讀取 因為CourseID是PK會自動新增
-                db.Course.Remove(new Course() { CourseID = 12 });
-                 * */
-                var c12 = db.Course.Find(12);
-                db.Course.Remove(c12);
-                db.SaveChanges();
-
-                //更新資料
-                foreach (var item in db.Course)
-                {
-                    item.Credits += 1;
-                }
-                db.SaveChanges();
             }
-           
+
 
         }
+        /// <summary>
+        /// 基本查詢
+        /// </summary>
+        /// <param name="db"></param>
+        static void Select(ContosoUniversityEntities db)
+        {
+            Console.WriteLine("基本查詢");
+            //基本
+            foreach (var item in db.Course)
+            {
+                Console.WriteLine(item.Title);
+            }
+        }
+        /// <summary>
+        /// 取出課程名稱中有Git的 用LINQ
+        /// </summary>
+        /// <param name="db"></param>
+        static void SelectByLinq(ContosoUniversityEntities db)
+        {
+
+            Console.WriteLine("SelectByLinq");
+            var data = from p in db.Course
+                       where p.Title.Contains("Git")
+                       select p;
+            foreach (var item in data)
+            {
+                Console.WriteLine(item.Title);
+
+            }
+        }
+
+        /// <summary>
+        /// 新增欄位
+        /// </summary>
+        /// <param name="db"></param>
+        public static void Insert(ContosoUniversityEntities db)
+        {
+            Console.WriteLine("Insert");
+            //新增欄位
+            db.Course.Add(new Course()
+            {
+                Title = "Git Test",
+                Credits = 5,
+                DepartmentID = 1
+            });
+
+            db.SaveChanges();
+
+        }
+        /// <summary>
+        /// 新增欄位ByFind Find只能找主鍵
+        /// </summary>
+        /// <param name="db"></param>
+        public static void InsertByFind(ContosoUniversityEntities db)
+        {
+            Console.WriteLine("InsertByFind");
+            //新增欄位
+            var c = new Course()
+            {
+                Title = "Git Test 2",
+                Credits = 5,
+            };
+            c.Department = db.Department.Find(2);
+            //更新前
+            Console.WriteLine(c.DepartmentID);
+
+            db.Course.Add(c);
+
+            //更新後
+            db.SaveChanges();
+            Console.WriteLine(c.DepartmentID);
+
+        }
+
+        /// <summary>
+        /// 更新資料
+        /// </summary>
+        /// <param name="db"></param>
+        public static void Update(ContosoUniversityEntities db)
+        {
+            Console.WriteLine("Update");
+            var c12 = db.Course.Find(12);
+
+            c12.Credits += 1;
+            db.SaveChanges();
+
+        }
+
+        /// <summary>
+        /// 刪除資料
+        /// </summary>
+        /// <param name="db"></param>
+        public static void Delete(ContosoUniversityEntities db)
+        {
+            Console.WriteLine("Delete");
+            ////刪除資料
+            ///*
+            // * 此句會刪除一筆最新的資料 但不是CourseID編號12 new時並沒有在new時被讀取 因為CourseID是PK會自動新增
+            //db.Course.Remove(new Course() { CourseID = 12 });
+            // * */
+
+            //var c12 = db.Course.Find(12);
+
+            //db.Course.Remove(c12);
+            //db.SaveChanges();
+
+            //刪除CourseID的資料
+            var dataDel = from p in db.Course
+                          where p.Title.Contains("Git Test")
+                          select p;
+
+            foreach (var item in dataDel)
+            {
+                db.Course.Remove(item);
+            }
+            db.SaveChanges();
+
+        }
+
 
     }
 }
