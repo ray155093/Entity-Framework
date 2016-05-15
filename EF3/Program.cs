9544a7db6776eff9dd54ca3a028f6f02886327ad
ray155093 <ray155093@gmail.com>
2016-05-15 12:53:30 +0800

    加入並行模式的兩個測試專案

---
+++ EF3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("App3");
+            using (var db = new ContosoUniversityEntities())
+            {
+                db.Database.Log = Console.WriteLine;
+                var c = db.Department.Find(1);
+
+                c.Name += "!!";
+                Console.ReadLine();
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("rowVersion錯誤");
+                    throw;
+                }
+                
+            }
+        }
+    }
+}
