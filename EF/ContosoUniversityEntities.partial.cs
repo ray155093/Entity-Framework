d6004e6199db9294557b40c75465b232ad6b6f7e
ray155093 <ray155093@gmail.com>
2016-05-15 10:33:36 +0800

    加入 partial ContosoUniversityEntities .cs override savechange() 自動填入ModifiedOn 欄位值 順便加入course.partial.cs 之前忘記加入

---
+++ EF/ContosoUniversityEntities.partial.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+namespace EF
+{
+    public partial class ContosoUniversityEntities : DbContext
+    {
+        public override int SaveChanges()
+        {
+            var Entities = this.ChangeTracker.Entries();
+
+            foreach (var entity in Entities)
+            {
+                switch (entity.State)
+                {
+                    case EntityState.Added:
+                        break;
+                    case EntityState.Deleted:
+                        break;
+                    case EntityState.Detached:
+                        break;
+                    case EntityState.Modified:
+                        break;
+                    case EntityState.Unchanged:
+                        break;
+                    default:
+                        break;
+                }
+                {
+ 
+                }
+                if (entity.Entity is Course)
+                {
+                    entity.CurrentValues.SetValues(new
+                    {
+                        ModifiedOn = DateTime.Now
+                    });
+                }
+            }
+            return base.SaveChanges();
+        }
+    }
+}
